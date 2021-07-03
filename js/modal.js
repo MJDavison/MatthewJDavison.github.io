404e89f89b0de05686e7ab6744e9ae2fd48591cd
matthewjdavison <matthewjdavison1+personalgithub@outlook.com>
2021-07-03 01:16:07 +0100

    Portfolio Published

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,14 @@
 const portfolioItems = document.querySelector(".projects");
 const resumesLink = document.querySelector("#resumesLink");
+console.log(resumesLink);
 var closeButton;
 let modalOpen = false;
 function openModal(e) {
-    e.preventDefault();
-    if (!modalOpen) {
+  e.preventDefault();
+  if (!modalOpen) {
     const caption = e.target.querySelector(".project__caption");
     if (!caption) return;
-    const modalToggle = caption.querySelector(".project__link");    
+    const modalToggle = caption.querySelector(".project__link");
     if (!modalToggle) return;
 
     let modal = caption.nextElementSibling;
@@ -17,21 +18,44 @@ function openModal(e) {
     document.body.style.overflowY = "hidden";
     document.body.tabIndex = -"1";
 
-    closeButton = modal.querySelector('.close-modal');
+    closeButton = modal.querySelector(".close-modal");
     closeButton.addEventListener("click", (_) => {
-      
       modal.classList.remove("modal-open");
       document.body.style.overflowY = "visible";
       document.body.tabIndex = "0";
       modalOpen = false;
     });
-  }
-  else{
-      console.log(modalOpen);
+  } else {
+    console.log(modalOpen);
   }
 }
+function openResumesModal(e) {
+  e.preventDefault();
+  if(!modalOpen){
 
+    let modal = e.target.parentElement.nextElementSibling;
+    if(!modal) return;
+    
+    console.log(modal);
+    modal.classList.add("modal-open");
+    modalOpen = true;
+    document.body.style.overflowY = "hidden";
+    document.body.tabIndex = -"1";
+
+
+
+    closeButton = modal.querySelector(".close-modal");
+    closeButton.addEventListener("click", (_) => {
+      modal.classList.remove("modal-open");
+      document.body.style.overflowY = "visible";
+      document.body.tabIndex = "0";
+      modalOpen = false;
+  });
+}
+}
 portfolioItems.addEventListener("click", (e) => openModal(e));
-resumesLink.addEventListener("click", (e) => openModal(e));
+// resumesLink.addEventListener("click", (e) => openModal(e));
+resumesLink.addEventListener("click", (e) => openResumesModal(e));
+resumesLink.addEventListener("click", e => console.log(e));
 
 // console.log(portfolioItems);
