c0954e04a4bce741c91b55884dbd9eaf21c654fe
matthewjdavison <matthewjdavison1+personalgithub@outlook.com>
2021-07-04 04:51:52 +0100

    Restructured folders and refactored lots of SCSS

--- js/index.js
+++ js/index.js
@@ -1,44 +1,5 @@
-const navigationButton = document.querySelector(".navigation-button");
-const nav = document.querySelector(".hamburger");
-
-const nav__svg = document.querySelector(".hamburger__svg");
-const home_button = document.querySelector("#nav__home")
-// nav__svg.style="stroke:white";
-// const portfolio_section = document.querySelector(".portfolio");
-// const about_section = document.querySelector(".about");
-const toggleNav = _ => {
-    document.body.classList.toggle('nav-open');
-};
-
-navigationButton.addEventListener("click", _ => toggleNav());
-home_button.addEventListener("click", e => {
-    e.preventDefault();
-    console.log(e);
-    document.body.scrollTo({top:0, behavior:'smooth'})
+const navToggle = document.querySelector('.navigation-button');
+const openClass = 'nav-open';
+navToggle.addEventListener('click', (e) => {
+    document.body.classList.toggle(openClass);
 })
-
-// const portfolio__sec__top = portfolio_section.getBoundingClientRect().top ;
-// const about__sec__top = about_section.getBoundingClientRect().top - 15-12;
-
-
-// console.log(portfolio_section.getBoundingClientRect());
-// console.log(about_section.getBoundingClientRect());
-
-// document.body.addEventListener("scroll", e => {
-//     console.log(document.body.scrollTop);
-//     // console.log("Test");
-//     // console.log(document.body.scrollTop);
-//     // console.log(cta__sec);
-//     if(document.body.scrollTop >= portfolio__sec__top){
-//         nav__svg.style="stroke:black";
-//         //console.log(nav__svg.style);
-//         if(document.body.scrollTop >= about__sec__top){
-//             nav__svg.style="stroke:white";
-//         }  
-//     } 
-//     else{
-//         nav__svg.style="stroke:white";
-//     }
-// });
-
-
--- js/modal.js
+++ js/modal.js
@@ -1,59 +1,28 @@
 const portfolioItems = document.querySelector(".projects");
 const resumesLink = document.querySelector("#resumesLink");
-// console.log(resumesLink);
-var closeButton;
-let modalOpen = false;
-function openModal(e) {
-  e.preventDefault();
-  
-  if (!modalOpen) {
-    const modalToggle = e.target.closest(".project__link");
-    if (!modalToggle) return;
-    const caption = modalToggle.parentElement;
-    let modal = caption.nextElementSibling;
-    modal.classList.add("modal-open");
-    modalOpen = true;
-    document.body.style.overflowY = "hidden";
-    document.body.tabIndex = -"1";
-
-    closeButton = modal.querySelector(".close-modal");
-    closeButton.addEventListener("click", (_) => {
-      modal.classList.remove("modal-open");
-      document.body.style.overflowY = "visible";
-      document.body.tabIndex = "0";
-      modalOpen = false;
-    });
-  } else {
-    console.log(modalOpen);
-  }
+let anyModalOpen = false;
+function openModal(e){
+if (!anyModalOpen) {
+        const modalToggle = e.target.closest('.project__link');
+        // console.log(modalToggle);
+        if (modalToggle) {
+            const modal = modalToggle.parentElement.nextElementSibling;
+            console.log(modal);
+            modal.classList.add('open');
+            document.body.style.overflowY= "hidden";    
+            anyModalOpen = true;
+            const close = modal.children[0];
+            console.log(close);
+            close.addEventListener("click", _ => {
+                modal.classList.remove('open');
+                anyModalOpen = false;
+                document.body.style.overflowY= "scroll";    
+            })
+        }
+    }   
 }
-function openResumesModal(e) {
-  e.preventDefault();
-  if(!modalOpen){
 
-    let modal = e.target.parentElement.nextElementSibling;
-    if(!modal) return;
-    
-    console.log(modal);
-    modal.classList.add("modal-open");
-    modalOpen = true;
-    document.body.style.overflowY = "hidden";
-    document.body.tabIndex = -"1";
-
-
-
-    closeButton = modal.querySelector(".close-modal");
-    closeButton.addEventListener("click", (_) => {
-      modal.classList.remove("modal-open");
-      document.body.style.overflowY = "visible";
-      document.body.tabIndex = "0";
-      modalOpen = false;
-  });
-}
-}
 portfolioItems.addEventListener("click", (e) => openModal(e));
-// resumesLink.addEventListener("click", (e) => openModal(e));
 resumesLink.addEventListener("click", (e) => openResumesModal(e));
-resumesLink.addEventListener("click", e => console.log(e));
 
-// console.log(portfolioItems);
+resumesLink.addEventListener("click", e => console.log(e));
